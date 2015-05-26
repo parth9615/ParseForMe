c7653d64e04bf6ca260c50404c57487805b6e5ef
Joel Wasserman <joel.wasserman8@gmail.com>
2015-05-26 17:09:04 -0400

    update on form and loging in/out

--- ParseForMe.py
+++ ParseForMe.py
@@ -1,9 +1,38 @@
+import cgi
+import urllib
+
+from google.appengine.api import users
+
 import webapp2
+
+MAIN_PAGE_TEMPLATE = """\
+    <form action="/sign?" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Parse For Me"></div>
+    </form>
+    <hr>
+    <a href="%s">%s</a>
+  </body>
+</html>
+"""
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Parse this for me please')
+
+        self.response.write('<hitml><body>')
+        # checks for active google account session
+        user = users.get_current_user()
+
+        # if there is a user already signed in, it is used here
+        if user:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext= 'Login'
+
+        self.response.write(MAIN_PAGE_TEMPLATE % (url, url_linktext))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
