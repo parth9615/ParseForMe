9693edf8f482057e65b06fd96c32c8cb1267c883
LoganGirard <harmmatan@gmail.com>
2015-07-23 22:36:02 -0700

    lots of server boilerplate, have fun Joel

---
+++ parseServer/server/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    res.render('index', { title: 'ejs' });
+});
+
+router.get('/derp', function(req, res, next) {
+  res.render('index', { title: 'ejs' });
+});
+
+/**
+ * Create file upload
+ */
+exports.create = function (req, res, next) {
+    var data = _.pick(req.body, 'type')
+        , uploadPath = path.normalize(cfg.data + '/uploads')
+        , file = req.files.file;
+
+        console.log(file.name); //original name (ie: sunset.png)
+        console.log(file.path); //tmp path (ie: /tmp/12345-xyaz.png)
+    console.log(uploadPath); //uploads directory: (ie: /home/user/data/uploads)
+};
+
+module.exports = router;
