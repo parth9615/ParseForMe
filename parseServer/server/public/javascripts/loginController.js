b3ee375fb2cbab82806c4dadf054973e31bd8f95
LoganGirard <harmmatan@gmail.com>
2015-07-28 00:52:45 -0700

    messy working version of login and base file upload. Files not uploading to parse yet. Cleaning up some first.

--- parseServer/server/public/javascripts/loginController.js
+++ parseServer/server/public/javascripts/loginController.js
@@ -1,6 +1,6 @@
 Parse.initialize("D66UUzuPDgCQ4Fxea73VbPxahF9xGZntWZ8mVlKT", "XSZ5x97qzUWiALEbAKUq49QlcKKPxuJlhmOedEEj");
 
-angular.module('AuthApp', [])
+var loginApp = angular.module('AuthApp', ['ngFileUpload'])
 .run(['$rootScope', function($scope) {
   $scope.scenario = 'Sign up';
   $scope.currentUser = Parse.User.current();
@@ -38,4 +38,34 @@ angular.module('AuthApp', [])
     Parse.User.logOut();
     $scope.currentUser = null;
   };
+
+  // $scope.$watch('files', function () {
+  //     $scope.upload($scope.files);
+  //   }
+}]);
+
+loginApp.controller('dragDropController', ['$scope', 'Upload', function ($scope, Upload) {
+    $scope.$watch('files', function () {
+        $scope.upload($scope.files);
+    });
+
+    $scope.upload = function (files) {
+        if (files && files.length) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                Upload.upload({
+                    url: 'uploads/',
+                    fields: {'username': $scope.username},
+                    file: file
+                }).progress(function (evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+                }).success(function (data, status, headers, config) {
+                    console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+                }).error(function (data, status, headers, config) {
+                    console.log('error status: ' + status);
+                })
+            }
+        }
+    };
 }]);
