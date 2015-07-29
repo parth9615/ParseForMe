71f1e0d3c0f7b7afeb7b009cf1e5e1b67500f771
LoganGirard <harmmatan@gmail.com>
2015-07-28 17:36:21 -0700

    working file upload to parse. next is associating it to a user

--- parseServer/server/controllers/UserController.js
+++ parseServer/server/controllers/UserController.js
@@ -1,3 +1,5 @@
+
+
 var fs = require('fs');
 
 UserController = function() {};
@@ -5,16 +7,17 @@ UserController = function() {};
 UserController.prototype.uploadFile = function(req, res) {
     // We are able to access req.files.file thanks to
     // the multiparty middleware
+
     var file = req.files.file;
     console.log(file.name);
     console.log(file.type);
-
+    console.log(req.body);
     fs.writeFile('./uploads/'+file.name, file, function(err) {
       if (err) throw err;
       console.log('It\'s saved');
     })
 
-    
+
 }
 
 module.exports = new UserController();
--- parseServer/server/public/javascripts/loginController.js
+++ parseServer/server/public/javascripts/loginController.js
@@ -1,6 +1,6 @@
 Parse.initialize("D66UUzuPDgCQ4Fxea73VbPxahF9xGZntWZ8mVlKT", "XSZ5x97qzUWiALEbAKUq49QlcKKPxuJlhmOedEEj");
 
-var loginApp = angular.module('AuthApp', ['ngFileUpload'])
+var loginApp = angular.module('AuthApp', ['ngFileUpload', 'ng'])
 .run(['$rootScope', function($scope) {
   $scope.scenario = 'Sign up';
   $scope.currentUser = Parse.User.current();
@@ -38,34 +38,47 @@ var loginApp = angular.module('AuthApp', ['ngFileUpload'])
     Parse.User.logOut();
     $scope.currentUser = null;
   };
-
-  // $scope.$watch('files', function () {
-  //     $scope.upload($scope.files);
-  //   }
 }]);
 
-loginApp.controller('dragDropController', ['$scope', 'Upload', function ($scope, Upload) {
+loginApp.controller('dragDropController', ['$scope', 'Upload', '$http', function ($scope, Upload, $http) {
     $scope.$watch('files', function () {
-        $scope.upload($scope.files);
+        $scope.upload($scope.files, $http);
     });
 
     $scope.upload = function (files) {
-        if (files && files.length) {
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-                Upload.upload({
-                    url: 'uploads/',
-                    fields: {'username': $scope.username},
-                    file: file
-                }).progress(function (evt) {
-                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-                }).success(function (data, status, headers, config) {
-                    console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
-                }).error(function (data, status, headers, config) {
-                    console.log('error status: ' + status);
-                })
-            }
-        }
-    };
+        if(files && files.length) {
+          var file = files[0];
+        $http.post("https://api.parse.com/1/files/"+file.name, file, {
+
+               headers: {
+                   'X-Parse-Application-Id': 'D66UUzuPDgCQ4Fxea73VbPxahF9xGZntWZ8mVlKT',
+                   'X-Parse-REST-API-Key': 'exvs87UNQZa5IVOCiJMnOuk28KzSJf47OGOwr7xF',
+                   'Content-Type': file.type
+               },
+               transformRequest: angular.identity
+
+        }).then(function(data) {
+          console.log(data.data.name);
+        })
+        };
+      }
+
+    //     if (files && files.length) {
+    //         for (var i = 0; i < files.length; i++) {
+    //             var file = files[i];
+    //             Upload.upload({
+    //                 url: 'uploads/',
+    //                 fields: {'username': $scope.username},
+    //                 file: file
+    //             }).progress(function (evt) {
+    //                 var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+    //                 console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
+    //             }).success(function (data, status, headers, config) {
+    //                 console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+    //             }).error(function (data, status, headers, config) {
+    //                 console.log('error status: ' + status);
+    //             })
+    //         }
+    //     }
+
 }]);
