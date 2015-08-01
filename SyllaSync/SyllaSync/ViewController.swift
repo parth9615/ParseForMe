22391b33c6628aaab0725875e4ba319b9d2a2603
Joel Wasserman <joel.wasserman8@gmail.com>
2015-08-01 10:24:46 -0700

    Added new xcode project with correct name

---
+++ SyllaSync/SyllaSync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SyllaSync
+//
+//  Created by Joel Wasserman on 8/1/15.
+//  Copyright (c) 2015 IVET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
