e39c4aab0ff66d7e4eeee2e29939b9bb563cae3e
Joel Wasserman <joel.wasserman8@gmail.com>
2015-08-17 15:11:03 -0400

    Changes to cloud code

--- SyllaSyncCloudCode/cloud/main.js
+++ SyllaSyncCloudCode/cloud/main.js
@@ -1,8 +1,8 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  var query = new Parse.Query('Events');
+Parse.Cloud.define("hello", function(request, response, date) {
+  var query = new Parse.Query(Parse.installation);
   query.equalTo('username', 'joelwass');
   Parse.Push.send({
     where: query, //set to our installation query
