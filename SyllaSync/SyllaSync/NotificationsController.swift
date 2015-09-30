e7cecddaa741878c39fc5b81a5709b6ba8bf1d65
Joel Wasserman <joel.wasserman8@gmail.com>
2015-09-30 19:40:48 -0400

    Need to change notifications controller to how its supposed to be

--- SyllaSync/SyllaSync/NotificationsController.swift
+++ SyllaSync/SyllaSync/NotificationsController.swift
@@ -8,34 +8,179 @@
 
 import UIKit
 
-class NotificationsController: UIViewController {
+class NotificationsController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
     @IBOutlet weak var classmatesTable: UITableView!
-    @IBOutlet weak var compareTextField: UITextField!
+    @IBOutlet weak var notificationsTableView: UITableView!
+    //
+    //  HamburgerController.swift
+    //  SyllaSync
+    //
+    //  Created by Joel Wasserman on 8/1/15.
+    //  Copyright (c) 2015 IVET. All rights reserved.
+    //
     
-    
-    //have a text field where they can enter either an email or a phone number
-    // submit button where they can send the compare invite
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        //TODO this is where you get the current classmates from parse
-        //TODO create a new table in parse. classmates will be mapped to a username then we can request a query to that specific database and 
-    }
-
-    @IBAction func compare(sender: AnyObject) {
-        //TODO implelment compare algo by comparing syllabi on Parse
+        //self.tableView.scrollEnabled = false //comment to enable scrolling
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 8
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.row == HamburgerCells.Filler.rawValue {
+            var cellIdentifier = "Filler"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+            }
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            cell?.backgroundColor = UIColor(rgba: "#04a4ca")
+            return cell!
+        }
+        else if indexPath.row == HamburgerCells.Calendar.rawValue {
+            var cellIdentifier = "Toggle"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? ToggleCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier) as? ToggleCell
+            }
+
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            return cell!
+        }
+        else if indexPath.row == HamburgerCells.Notifications.rawValue {
+            var cellIdentifier = "Compare"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+            }
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            return cell!
+            
+        }
+        else if indexPath.row == HamburgerCells.Settings.rawValue {
+            var cellIdentifier = "Settings"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+            }
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            return cell!
+        }
+        else if indexPath.row == HamburgerCells.ViewSyllabi.rawValue {
+            var cellIdentifier = "ViewSyllabi"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+            }
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            return cell!
+            
+        }
+        else if indexPath.row == HamburgerCells.AboutUs.rawValue {
+            var cellIdentifier = "AboutUs"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+            }
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            return cell!
+        }
+        else if indexPath.row == HamburgerCells.Logout.rawValue {
+            var cellIdentifier = "Logout"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier:cellIdentifier)
+            }
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            return cell!
+        }
+        else {
+            var cellIdentifier = "Fluff"
+            var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath:indexPath) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+            }
+            cell?.selectionStyle = UITableViewCellSelectionStyle.None
+            return cell!
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.row == HamburgerCells.Filler.rawValue {
+            return 175
+        }
+        if indexPath.row == HamburgerCells.ViewSyllabi.rawValue || indexPath.row == HamburgerCells.Notifications.rawValue || indexPath.row == HamburgerCells.Settings.rawValue || indexPath.row == HamburgerCells.AboutUs.rawValue || indexPath.row == HamburgerCells.Logout.rawValue || indexPath.row == HamburgerCells.Calendar.rawValue {
+            return 45
+        }
+        else {
+            return 500
+        }
+    }
+    
+    
+    //    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    //        // Return NO if you do not want the specified item to be editable.
+    //        return false
+    //    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)  {
+        if indexPath.row == HamburgerCells.Settings.rawValue {
+            //go to new page
+            var settingsVC = self.storyboard?.instantiateViewControllerWithIdentifier("Settings") as! SettingsController
+            self.presentViewController(settingsVC, animated: true, completion: nil)
+        }
+        else if indexPath.row == HamburgerCells.AboutUs.rawValue {
+            //go to new page
+            var aboutVC = self.storyboard?.instantiateViewControllerWithIdentifier("AboutUs") as! AboutController
+            self.presentViewController(aboutVC, animated: true, completion: nil)
+        }
+        else if indexPath.row == HamburgerCells.Notifications.rawValue {
+            
+            var notificationsVC = self.storyboard?.instantiateViewControllerWithIdentifier("Notifications") as! NotificationsController
+            self.presentViewController(notificationsVC, animated: true, completion: nil)
+        }
+        else if indexPath.row == HamburgerCells.ViewSyllabi.rawValue {
+            
+            var inviteVC = self.storyboard?.instantiateViewControllerWithIdentifier("Syllabi") as! SyllabiController
+            self.presentViewController(inviteVC, animated: true, completion: nil)
+        }
+        else if indexPath.row == HamburgerCells.Logout.rawValue {
+            
+            var logoutVC = self.storyboard?.instantiateViewControllerWithIdentifier("Logout") as! LogoutController
+            self.presentViewController(logoutVC, animated: true, completion: nil)
+        }
+        else {
+            
+            
+        }
+    }
+    
+    
     
     @IBAction func dismiss(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
