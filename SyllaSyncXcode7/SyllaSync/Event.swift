d64848e77c620d69907503e9d3a7e5839e424c90
Joel Wasserman <joel.wasserman8@gmail.com>
2015-10-21 15:04:50 -0400

    refactored event service to utilize event objects, much cleaner now

--- SyllaSyncXcode7/SyllaSync/Event.swift
+++ SyllaSyncXcode7/SyllaSync/Event.swift
@@ -10,68 +10,15 @@ import UIKit
 
 public class Event: NSObject {
     
-    public var type: String? {
-        get {
-            return self.type
-        }
-        set(newType) {
-            self.type = newType
-        }
-    }
+    public var UUID:String?
+    public var type:String?
+    public var date:String?
+    public var time:String?
+    public var title:String?
+    public var weight:Int?
+    public var syllabus:String?
+    public var className:String?
     
-    public var date:String? {
-        get {
-            return self.date
-        }
-        set (newDate) {
-            self.date = newDate
-        }
-    }
-
-    public var time: String? {
-        get {
-            return self.time
-        }
-        set(newTime) {
-            self.time = newTime
-        }
-    }
-    
-    public var title:String? {
-        get {
-            return self.title
-        }
-        set (newTitle) {
-            self.title = newTitle
-        }
-    }
-
-    public var weight: Int? {
-        get {
-            return self.weight
-        }
-        set(newWeight) {
-            self.weight = newWeight
-        }
-    }
-    
-    public var syllabus:String? {
-        get {
-            return self.syllabus
-        }
-        set (newSyllabus) {
-            self.syllabus = newSyllabus
-        }
-    }
-    
-    public var className:String? {
-        get {
-            return self.className
-        }
-        set (newClassName) {
-            self.className = newClassName
-        }
-    }
 }
 
 
--- SyllaSyncXcode7/SyllaSync/EventService.swift
+++ SyllaSyncXcode7/SyllaSync/EventService.swift
@@ -18,17 +18,13 @@ public struct EventServiceConstants {
 
 public class EventService: NSObject {
     
-    var eventsArray:NSMutableArray?
+    var eventsFromParse:NSMutableArray?
+    var eventsArray = [Event]()
     
-    var eventsArrayTypes = [String]()
-    var eventsArrayDates = [String]()
-    var eventsArrayTimes = [String]()
-    var eventsArrayTitles = [String]()
-    var eventsArrayWeights = [Int]()
-    var eventsArraySyllabus = [String]()
-    var eventsArrayCount:Array<Int> = [0]
-    var headerCount:Int = 0
-//    var json:JSON?
+    var uniqueClassCount = 0
+    var uniqueClasses = [String]()
+    
+    //    var json:JSON?
     public class var sharedInstance : EventService {
         return EventServiceInstance
     }
@@ -45,11 +41,11 @@ public class EventService: NSObject {
                 print("got a query for \(UserSettings.sharedInstance.Username)")
                 if let objects = objects as? [PFObject!] {
                     
-                    self.eventsArray = NSMutableArray()
+                    self.eventsFromParse = NSMutableArray()
                     
-                    self.eventsArray!.addObjectsFromArray(objects)
+                    self.eventsFromParse!.addObjectsFromArray(objects)
                     
-                    self.getHeaderCount(sender)
+                    self.createEventObjects(sender)
                 }
                 else {
                     self.finish(sender)
@@ -61,189 +57,84 @@ public class EventService: NSObject {
         }
     }
     
-    
-    //Gets the number of syllabi (or at least attempts to)
-    func getHeaderCount(sender: AnyObject) {
-        var prevSyllabus:AnyObject?
-        
-        headerCount = 0
-        if eventsArray!.count == 0 {
-            self.finish(sender)
-        }
-        if eventsArray != nil {
-            if let event:AnyObject = eventsArray?[0] {
-                if let eventDetails:AnyObject = event["events"] {
-                    if let syllabus: AnyObject? = eventDetails["Syllabus"] {
-                        prevSyllabus = syllabus
-                        headerCount++
-                    }
+    func createEventObjects(sender: AnyObject) {
+        if eventsFromParse != nil {
+            for each in eventsFromParse! {
+                if let eventDetails:AnyObject = each["events"] {
+                    
+                    let event = Event()
+                    event.className = eventDetails["Classname"] as? String
+                    event.date = eventDetails["Date"] as? String
+                    event.syllabus = eventDetails["Syllabus"] as? String
+                    event.time = eventDetails["Time"] as? String
+                    event.title = eventDetails["Title"] as? String
+                    event.weight = (eventDetails["Weight"] as! NSNumber) as Int
+                    event.UUID = event.className!+event.date!+event.title!
+                    
+                    eventsArray.append(event)
                 }
             }
         }
-        
-        if eventsArray != nil {
-            print("eventsArray count: \(eventsArray!.count)")
-            for var i = 0; i < eventsArray!.count; i++ {
-                if let event: AnyObject = eventsArray?[i] {
-                    if let eventDetails:AnyObject = event["events"] {
-                        if let syllabus: AnyObject = eventDetails["Syllabus"] {
-                            if syllabus.isEqual(prevSyllabus) {
-                                continue
-                            }
-                            else {
-                                eventsArraySyllabus.append("\(syllabus)")
-                                headerCount++
-                                prevSyllabus = syllabus
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        createEventsArray(sender)
+        countUniqueClasses(sender)
     }
     
-    
-    
-    func createEventsArray(sender: AnyObject) {
-        if headerCount != eventsArrayCount.count {
-            for var i = 0; i < headerCount; i++ { // used to be i=1 start... trying to fix issue
-                eventsArrayCount.append(0)
-            }
-            getEventsPerHeader(sender)
-        }
-        finish(sender)
-    }
-    
-    func getEventsPerHeader(sender: AnyObject) {
-        var firstSyllabus:AnyObject?
-        var prevSyllabus:AnyObject?
-        
-        var tmpHeaderCount = 0
-        if eventsArray != nil {
-            if let event:AnyObject = eventsArray?[0] {
-                if let eventDetails:AnyObject = event["events"] {
-                    if let syllabus: AnyObject? = eventDetails["Syllabus"] {
-                        firstSyllabus = syllabus
-                        prevSyllabus = firstSyllabus
-                    }
+    func countUniqueClasses(sender: AnyObject) {
+        for each in eventsArray {
+            var duplicateFlag = false
+            for classNames in uniqueClasses {
+                if each.className == classNames {
+                    duplicateFlag = true
+                    continue
                 }
             }
-        }
-        
-        if eventsArray != nil {
-            for var i = 0; i < eventsArray!.count; i++ {
-                if let event: AnyObject = eventsArray?[i] {
-                    if let eventDetails:AnyObject = event["events"] {
-                        if let syllabus: AnyObject = eventDetails["Syllabus"] {
-                            if syllabus.isEqual(prevSyllabus) {
-                                eventsArrayCount[tmpHeaderCount]++
-                                
-                                if let eventType:AnyObject = eventDetails["Type"] {
-                                    eventsArrayTypes.append("\(eventType)")
-                                }
-                                if let eventDate:AnyObject = eventDetails["Date"] {
-                                    eventsArrayDates.append("\(eventDate)")
-                                }
-                                if let eventTime:AnyObject = eventDetails["Time"] {
-                                    eventsArrayTimes.append("\(eventTime)")
-                                }
-                                if let eventTitle:AnyObject = eventDetails["Title"] {
-                                    eventsArrayTitles.append("\(eventTitle)")
-                                }
-                                if let eventWeight:AnyObject = eventDetails["Weight"] {
-                                    eventsArrayWeights.append(eventWeight as! Int)
-                                }
-                                if let eventSyllabus:AnyObject = eventDetails["Syllabus"] {
-                                    eventsArraySyllabus.append("\(eventSyllabus)")
-                                }
-                                
-                                //schedule notifications
-                                scheduleNotification(eventDetails)
-                                
-                                continue
-                            }
-                            else {
-                                tmpHeaderCount++
-                                eventsArrayCount[tmpHeaderCount]++
-                                prevSyllabus = syllabus
-                                
-                                if let eventType:AnyObject = eventDetails["Type"] {
-                                    eventsArrayTypes.append("\(eventType)")
-                                }
-                                if let eventDate:AnyObject = eventDetails["Date"] {
-                                    eventsArrayDates.append("\(eventDate)")
-                                }
-                                if let eventTime:AnyObject = eventDetails["Time"] {
-                                    eventsArrayTimes.append("\(eventTime)")
-                                }
-                                if let eventTitle:AnyObject = eventDetails["Title"] {
-                                    eventsArrayTitles.append("\(eventTitle)")
-                                }
-                                if let eventWeight:AnyObject = eventDetails["Weight"] {
-                                    eventsArrayWeights.append(eventWeight as! Int)
-                                }
-                                if let eventSyllabus:AnyObject = eventDetails["Syllabus"] {
-                                    eventsArraySyllabus.append("\(eventSyllabus)")
-                                }
-                                
-                                //schedule notifications
-                                scheduleNotification(eventDetails)
-                            }
-                        }
-                    }
-                }
+            if !duplicateFlag {
+                uniqueClasses.append(each.className!)
             }
         }
-        finish(sender)
+        scheduleNotification(sender)
     }
     
-    
-    func scheduleNotification(eventDetails: AnyObject) {
+    func scheduleNotification(sender: AnyObject) {
         let twoWeekNotification = UILocalNotification()
         let oneWeekNotification = UILocalNotification()
         let dayBeforeNotification = UILocalNotification()
-        let eventTitle:AnyObject? = eventDetails["Title"]
-        let eventSyllabus:AnyObject? = eventDetails["Syllabus"]
-        if let eventFireDate:AnyObject = eventDetails["Date"] {
-            if UserSettings.sharedInstance.notificationsScheduled["\(eventTitle!)\(eventSyllabus)\(eventFireDate)"] == nil {
-                let notificationUUID = "\(eventTitle!)\(eventSyllabus)\(eventFireDate)"
+        for each in eventsArray {
+            if UserSettings.sharedInstance.notificationsScheduled[each.UUID!] == nil {
                 
-                let eventFireDateString = "\(eventFireDate)"
-                let dateFromString = eventFireDateString.componentsSeparatedByString("/")
+                let eventFireDateString = each.date
+                let dateFromString = eventFireDateString!.componentsSeparatedByString("/")
                 let newCVDate = CVDate(day: Int(dateFromString[1])!, month: Int(dateFromString[0])!, week: ((Int(dateFromString[1])!)/7)+1, year: Int(dateFromString[2])!)
                 let finalDate = newCVDate.convertedDate()?.addHours(5)
                 
                 //two week prior notification
-                twoWeekNotification.userInfo = ["UUID": notificationUUID]
+                twoWeekNotification.userInfo = ["UUID": each.UUID!]
                 twoWeekNotification.fireDate = finalDate!.addDays(-14)
                 twoWeekNotification.timeZone = NSTimeZone.localTimeZone()
-                twoWeekNotification.alertBody = "Don't forget! You have \(eventTitle!) in just two weeks!"
+                twoWeekNotification.alertBody = "You have \(each.title!) in two weeks! Time to start studying!"
                 twoWeekNotification.soundName = UILocalNotificationDefaultSoundName
                 UIApplication.sharedApplication().scheduleLocalNotification(twoWeekNotification)
                 
                 //one week prior notification
-                oneWeekNotification.userInfo = ["UUID": notificationUUID]
+                oneWeekNotification.userInfo = ["UUID": each.UUID!]
                 oneWeekNotification.fireDate = finalDate!.addDays(-7)
                 oneWeekNotification.timeZone = NSTimeZone.localTimeZone()
-                oneWeekNotification.alertBody = "Oh boy... Only one week until \(eventTitle!). You can do it!"
+                oneWeekNotification.alertBody = "Only one week until \(each.title!). Keep up the hard work!"
                 oneWeekNotification.soundName = UILocalNotificationDefaultSoundName
                 UIApplication.sharedApplication().scheduleLocalNotification(oneWeekNotification)
                 
                 //one day prior notification
-                dayBeforeNotification.userInfo = ["UUID": notificationUUID]
+                dayBeforeNotification.userInfo = ["UUID": each.UUID!]
                 dayBeforeNotification.fireDate = finalDate!.addDays(-1)
                 dayBeforeNotification.timeZone = NSTimeZone.localTimeZone()
-                dayBeforeNotification.alertBody = "Tomorrow is the day for \(eventTitle!). Don't forget and good luck!"
+                dayBeforeNotification.alertBody = "You have \(each.title!) tomorrow. Good luck!"
                 dayBeforeNotification.soundName = UILocalNotificationDefaultSoundName
                 UIApplication.sharedApplication().scheduleLocalNotification(dayBeforeNotification)
                 
-                UserSettings.sharedInstance.notificationsScheduled["\(eventTitle!)\(eventSyllabus)\(eventFireDate)"] = true
+                UserSettings.sharedInstance.notificationsScheduled[each.UUID!] = true
             }
-            notificationsScheduled["\(eventTitle)\(eventSyllabus)\(eventFireDate)"] == true
-            //TODO use user settings that were just implemented to set the notifications that were set so that we don't set them more than once
-            print("notifications: \n\(UserSettings.sharedInstance.notificationsScheduled)", terminator: "")
+            notificationsScheduled[each.UUID!] == true
         }
+        finish(sender)
     }
     
     func finish(sender: AnyObject) {
@@ -264,28 +155,19 @@ public class EventService: NSObject {
     }
     
     func checkNoRepeatNotifications() {
-        //TODO iterate through the local class level notifications list and the user settings notifications list and remove any repeats and remove any that are no longer there.
-
+        
         for notification in UIApplication.sharedApplication().scheduledLocalNotifications! {
             var flag = false
             for each in UserSettings.sharedInstance.notificationsScheduled {
                 if notification.userInfo!["UUID"] as! String == each.0 {
                     flag = true
                 }
-                if !flag {
-                    UIApplication.sharedApplication().cancelLocalNotification(notification)
-                }
+            }
+            if !flag {
+                UIApplication.sharedApplication().cancelLocalNotification(notification)
             }
         }
         print("\n\n\n User Settings\(UserSettings.sharedInstance.notificationsScheduled)\n\n")
         print("UIApplication notifications \(UIApplication.sharedApplication().scheduledLocalNotifications)\n\n")
-
-        //TODO check if there are repeat notifications scheduled. there shouldn't be but could possibly be...
-        
-//        for each in UserSettings.sharedInstance.notificationsScheduled {
-//            if each == previousNotification! {
-//                UserSettings.sharedInstance.notificationsScheduled.removeAtIndex(each.0, each.1)
-//            }
-//        }
     }
 }
